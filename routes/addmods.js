cd002bc825b88b4763431aa19ccb40dfddd89863
VenkataPrabhakar <89261443+VenkataPrabhakar@users.noreply.github.com>
2021-11-02 00:32:12 -0500

    query params

--- routes/addmods.js
+++ routes/addmods.js
@@ -4,10 +4,10 @@ var router = express.Router();
 /* GET home page. */
 router.get('/', function(req, res, next) {
   let query = req.query 
-console.log(`rows ${query.rows}`) 
-console.log(`cols ${query.cols}`)
-var rows=Number(query.rows)
-    var cols=Number(query.cols)
+  console.log(`rows ${query.rows}`) 
+  console.log(`cols ${query.cols}`)
+  var rows=Number(query.rows)
+  var cols=Number(query.cols)
   res.render('addmods', { title: 'Add Mods',query: query });
 });
 
